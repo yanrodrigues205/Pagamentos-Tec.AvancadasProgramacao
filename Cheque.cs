4b263b6e1203e23b1ab6cc2e7334d271f320f19b
yanpablo205 <yan.pablo205@gmail.com>
2023-10-05 14:32:07 -0300

    first

---
+++ Cheque.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace List_DataStruct
+{
+    public class Cheque : Pagamento
+    {
+        private long numero;
+        private DateTime dataDeposito;
+        private int situacao;
+
+    }
+}
---
+++ Especie.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace List_DataStruct
+{
+    public class Especie : Pagamento
+    {
+        private double quantia;
+        private double troco;
+    }
+}
